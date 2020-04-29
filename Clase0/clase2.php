69c80ec24b7139a310010b4958da2a243472aed5
AaronVelasco93 <aaronvelasco1993@outlook.com>
2020-04-28 19:50:45 -0500

    Se agregan ejemplos de clase

---
+++ Clase0/clase2.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Estilos inicio</title>
+</head>
+<!--Estilos-->
+<style>
+    .miEstilo {
+        color: white;
+        background-color: black;
+        text-align: center;
+        font-size: 25px;
+        border: 10px solid green;
+
+
+        width: 50%;
+        margin-left: 30px;
+
+
+    }
+</style>
+
+<body>
+    <!--Contenedor 1-->
+    <div style="background-color: red;">
+        hola mundo
+    </div>
+    <div style="background-color: red;">
+        hola mundo
+    </div>
+    <div style="background-color: red;">
+        hola mundo
+    </div>
+    <div style="background-color: red;">
+        hola mundo
+    </div>
+    <div style="background-color: red;">
+        hola mundo
+    </div>
+    <div style="background-color: red;">
+        hola mundo
+    </div>
+    <div style="background-color: red;">
+        hola mundo
+        <div class="miEstilo">
+            Hola desde otro div
+        </div>
+    </div>
+
+    <div style="background-color: red;">
+        hola mundo
+    </div>
+
+    <!--Contenedor 2-->
+    <div class="miEstilo">
+        Hola desde otro div
+    </div>
+
+    <br>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus totam laborum animi aliquid, in mollitia quisquam fugit sit, facilis repellat ex asperiores. Accusamus iure amet soluta magni. Nemo, soluta aspernatur.</p>
+    <div class="miEstilo">
+        hola como estas
+    </div>
+
+
+
+</body>
+
+</html>
